a9eaf9977763d73eb4c27b8a737a4f9e260195e5
Sameer Sheorey <41028320+ssheorey@users.noreply.github.com>
2025-09-15 00:30:09 -0700

    Loading kernels now works in Windows.

--- gsplat/sycl/_backend.py
+++ gsplat/sycl/_backend.py
@@ -1,7 +1,24 @@
 _C = None
 
+import os
+
+import gsplat
+
+if os.name == "nt":
+    import torch
+    from sysconfig import get_path
+
+    dllpath = [
+        os.add_dll_directory(torch.__path__[0] + "/lib"),  # for torch libs
+        os.add_dll_directory(get_path("data") + "/Library/bin"),  # for sycl libs
+    ]
+
 try:
     # Try to import the compiled module (via setup.py or pre-built .so)
     from gsplat import gsplat_sycl_kernels as _C
-except ImportError: 
+except ImportError:
     raise ImportError("Unable to find compiled sycl kernels package")
+
+if os.name == "nt":
+    for dp in dllpath:
+        dp.close()
